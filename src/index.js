746feaa806091dc6494fb0c9bbb63f5c2a01e25b
Karol Czeryna <karol.czeryna@gmail.com>
2024-01-31 22:54:51 +0100

    publish to github packages

--- src/index.js
+++ src/index.js
@@ -3,47 +3,47 @@ import withMaterialSurfaces from "tailwind-material-surfaces";
 import withModeAwareColors from "tailwind-mode-aware-colors";
 
 export const withMaterialColors = (
-  config,
-  colorsMap,
-  options = { extend: false }
+    config,
+    colorsMap,
+    options = { extend: false }
 ) => {
-  if (colorsMap.primary) {
-    const materialColors = tailwindThemeFromColor(colorsMap);
+    if (colorsMap.primary) {
+        const materialColors = tailwindThemeFromColor(colorsMap);
 
-    return withModeAwareColors(
-      withMaterialSurfaces(
-        {
-          ...config,
-          theme: options.extend
-            ? {
-                ...(config.theme || {}),
-                extend: {
-                  ...(config.theme?.extend || {}),
-                  colors: {
-                    ...(config.theme?.extend?.colors || {}),
-                    ...materialColors,
-                  },
+        return withModeAwareColors(
+            withMaterialSurfaces(
+                {
+                    ...config,
+                    theme: options.extend
+                        ? {
+                            ...(config.theme || {}),
+                            extend: {
+                                ...(config.theme?.extend || {}),
+                                colors: {
+                                    ...(config.theme?.extend?.colors || {}),
+                                    ...materialColors,
+                                },
+                            },
+                        }
+                        : {
+                            ...(config.theme || {}),
+                            colors: { ...(config.theme?.colors || {}), ...materialColors },
+                        },
                 },
-              }
-            : {
-                ...(config.theme || {}),
-                colors: { ...(config.theme?.colors || {}), ...materialColors },
-              },
-        },
-        {
-          surfacePrefix: "bg",
-          interactiveSurfacePrefix: "interactive-bg",
-          disabledStyles: {
-            textOpacity: 0.38,
-            backgroundOpacity: 0.12,
-            colorName: "on-surface",
-          },
-        }
-      )
-    );
-  } else {
-    throw "A primary color must be specified";
-  }
+                {
+                    surfacePrefix: "bg",
+                    interactiveSurfacePrefix: "interactive-bg",
+                    disabledStyles: {
+                        textOpacity: 0.38,
+                        backgroundOpacity: 0.12,
+                        colorName: "on-surface",
+                    },
+                }
+            )
+        );
+    } else {
+        throw "A primary color must be specified";
+    }
 };
 
 // TODO numbered surface colors
--- src/tailwindThemeFromColor.js
+++ src/tailwindThemeFromColor.js
@@ -1,9 +1,9 @@
 import {
-  argbFromHex,
-  hexFromArgb,
-  Scheme,
-  customColor,
-} from "@material/material-color-utilities";
+    argbFromHex,
+    hexFromArgb,
+    Scheme,
+    customColor,
+} from "@eEQK/material-color-utilities";
 import { kebabize } from "./kebabize";
 
 /**
@@ -11,60 +11,60 @@ import { kebabize } from "./kebabize";
  * @param {Record<string, string>} colorsMap Values must be hex colors and 'primary' must be present.
  */
 export const tailwindThemeFromColor = (colorsMap) => {
-  const { primary, ...extraColors } = colorsMap;
+    const { primary, ...extraColors } = colorsMap;
 
-  const source = argbFromHex(primary);
+    const source = argbFromHex(primary);
 
-  const lightScheme = Scheme.light(source);
-  const darkScheme = Scheme.dark(source);
+    const lightScheme = Scheme.light(source);
+    const darkScheme = Scheme.dark(source);
 
-  // fix inverse-on-surface which should be named on-inverse-surface
-  lightScheme.props["onInverseSurface"] = lightScheme.props.inverseOnSurface;
-  delete lightScheme.props.inverseOnSurface;
-  darkScheme["onInverseSurface"] = darkScheme.props.inverseOnSurface;
-  delete darkScheme.props.inverseOnSurface;
+    // fix inverse-on-surface which should be named on-inverse-surface
+    lightScheme.props["onInverseSurface"] = lightScheme.props.inverseOnSurface;
+    delete lightScheme.props.inverseOnSurface;
+    darkScheme["onInverseSurface"] = darkScheme.props.inverseOnSurface;
+    delete darkScheme.props.inverseOnSurface;
 
-  // default colors
-  const colors = {
-    transparent: "transparent",
-    current: "currentColor",
-    black: "#000000",
-    white: "#ffffff",
-  };
+    // default colors
+    const colors = {
+        transparent: "transparent",
+        current: "currentColor",
+        black: "#000000",
+        white: "#ffffff",
+    };
 
-  Object.keys(lightScheme.props).forEach((colorName) => {
-    if (!["shadow", "scrim"].includes(colorName)) {
-      const kebabName = kebabize(colorName);
-      colors[`${kebabName}-light`] = hexFromArgb(lightScheme.props[colorName]);
-      colors[`${kebabName}-dark`] = hexFromArgb(darkScheme.props[colorName]);
-    }
-  });
-
-  Object.keys(extraColors).forEach((colorName) => {
-    const value = extraColors[colorName];
-
-    const hex = typeof value === "string" ? value : value.hex;
-    const blend = value === hex ? true : value.harmonize;
-
-    const { light, dark } = customColor(source, {
-      value: argbFromHex(hex),
-      blend,
+    Object.keys(lightScheme.props).forEach((colorName) => {
+        if (!["shadow", "scrim"].includes(colorName)) {
+            const kebabName = kebabize(colorName);
+            colors[`${kebabName}-light`] = hexFromArgb(lightScheme.props[colorName]);
+            colors[`${kebabName}-dark`] = hexFromArgb(darkScheme.props[colorName]);
+        }
     });
-    const kebabName = kebabize(colorName);
 
-    colors[`${kebabName}-light`] = hexFromArgb(light.color);
-    colors[`on-${kebabName}-light`] = hexFromArgb(light.onColor);
-    colors[`${kebabName}-container-light`] = hexFromArgb(light.colorContainer);
-    colors[`on-${kebabName}-container-light`] = hexFromArgb(
-      light.onColorContainer
-    );
-    colors[`${kebabName}-dark`] = hexFromArgb(dark.color);
-    colors[`on-${kebabName}-dark`] = hexFromArgb(dark.onColor);
-    colors[`${kebabName}-container-dark`] = hexFromArgb(dark.colorContainer);
-    colors[`on-${kebabName}-container-dark`] = hexFromArgb(
-      dark.onColorContainer
-    );
-  });
+    Object.keys(extraColors).forEach((colorName) => {
+        const value = extraColors[colorName];
 
-  return colors;
+        const hex = typeof value === "string" ? value : value.hex;
+        const blend = value === hex ? true : value.harmonize;
+
+        const { light, dark } = customColor(source, {
+            value: argbFromHex(hex),
+            blend,
+        });
+        const kebabName = kebabize(colorName);
+
+        colors[`${kebabName}-light`] = hexFromArgb(light.color);
+        colors[`on-${kebabName}-light`] = hexFromArgb(light.onColor);
+        colors[`${kebabName}-container-light`] = hexFromArgb(light.colorContainer);
+        colors[`on-${kebabName}-container-light`] = hexFromArgb(
+            light.onColorContainer
+        );
+        colors[`${kebabName}-dark`] = hexFromArgb(dark.color);
+        colors[`on-${kebabName}-dark`] = hexFromArgb(dark.onColor);
+        colors[`${kebabName}-container-dark`] = hexFromArgb(dark.colorContainer);
+        colors[`on-${kebabName}-container-dark`] = hexFromArgb(
+            dark.onColorContainer
+        );
+    });
+
+    return colors;
 };
